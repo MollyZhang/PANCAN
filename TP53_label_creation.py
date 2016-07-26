8308563df238c9ee51d17903c8329786e353eac6
Molly Zhang <mollyzhang@ucsc.edu>
2016-07-25 23:25:44 -0700

    add short barcode column and transpotion os cnv data

--- TP53_label_creation.py
+++ TP53_label_creation.py
@@ -1,20 +1,49 @@
+import pandas as pd
+
+
 SNV = "data/snv.tsv"
 SNV_TP53 = "data/snv_TP53.tsv"
+CNV_TP53 = "data/cnv_TP53.tsv"
+RNASEQ = "data/RNASeq.tsv"
+RNASEQ_BRCA = "data/by_tissue_RNASeq/RNASeq_BRCA.tsv"
+
 
 def main():
-    extract_TP53_from_snv()
+    cnv_TP53_tranposition()
+
+def create_label():
+    """add a TP53 + or - label to RNASeq data"""
+    brca_df = pd.read_csv(RNASEQ_BRCA, sep="\t")
+    snv_df = pd.read_csv(SNV_TP53, sep="\t")
+    print snv_df["Variant_Classification"]
+
 
 def extract_TP53_from_snv():
     f = open(SNV, "r")
     f_out = open(SNV_TP53, "w")
     for index, line in enumerate(f):
-        if index%100 == 0:
-            print index
+        if index == 0:
+            f_out.write(line.strip() + "\tshort_barcode\n")
+        if index%1000 == 0:
+            print "line", index
         gene = line.strip().split("\t")[60]
+        barcode = line.strip().split("\t")[40]
         if gene == "TP53":
-            f_out.write(line)
+            f_out.write(line.strip() + "\t{0}\n".format(barcode[:12]))
     f.close()
     f_out.close()
+
+def cnv_TP53_tranposition():
+    df = pd.read_csv(CNV_TP53, sep="\t", index_col = None)
+    df = df[df.columns[3:]]
+
+    df = df.transpose()
+    df.columns = ["cnv"]
+    df["sample_barcode"] = df.index
+    df["short_barcode"] = [i[:12] for i in df.sample_barcode]
+    new_cols = list(df.columns[1:]) + [df.columns[0]]
+    df = df[new_cols]
+    df.to_csv("data/cnv_TP53_transposed.tsv", index=None, sep="\t")
 
 
 if __name__ == '__main__':
